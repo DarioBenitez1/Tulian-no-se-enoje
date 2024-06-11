0906cd2f15ef16f7e98b047df3cf445709180399
PiernasNegras <martinezsnahu@gmail.com>
2024-06-11 18:42:48 -0300

    agrego las carreras que borre sin querer

---
+++ src/controllers/materia.controller.js
@@ -0,0 +1,73 @@
+[
+    {
+        "id":1,
+        "nombre":"Introduccion a la programacion",
+        "cuatrimestral":1,
+        "anio":1,
+        "carreraId" : 1
+    },
+    {
+        "id": 2,
+        "nombre":"Ingles 1",
+        "cuatrimestral":2,
+        "anio":1,
+        "carreraId" : 1
+    },
+    {
+       "id": 3,
+        "nombre":"Ingenieria de software 1",
+        "cuatrimestral":1,
+        "anio":2,
+        "carreraId" : 1
+    },
+    {
+        "id":4,
+        "nombre":"Base de datos 1",
+        "cuatrimestral":2,
+        "anio":2,
+        "carreraId" : 1
+    },
+
+    {
+        "id":5,
+        "nombre":"Introduccion al derecho",
+        "cuatrimestral":1,
+        "anio":1,
+        "carreraId" : 2
+    },
+    {
+        "id":6,
+        "nombre":"Economia politica",
+        "cuatrimestral":2,
+        "anio":1,
+        "carreraId" : 2
+    },
+    {
+        "id":7,
+        "nombre":"Derecho comercial 1",
+        "cuatrimestral":1,
+        "anio":2,
+        "carreraId" : 2
+    },
+    {
+        "id":8,
+        "nombre":"Derecho comercial 2",
+        "cuatrimestral":2,
+        "anio":2,
+        "carreraId" : 2
+    },
+    {
+        "id":9,
+        "nombre":"Contabilidad",
+        "cuatrimestral":1,
+        "anio":1,
+        "carreraId":3
+    },
+    {
+        "id":10,
+        "nombre":"Comercializacion",
+        "cuatrimestral":2,
+        "anio":2,
+        "carreraId":3
+    }
+]
